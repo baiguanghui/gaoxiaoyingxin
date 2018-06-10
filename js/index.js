260afbdcd526867284e36334d86d2486313a57a3
baiguanghui <724618014@qq.com>
2018-06-10 17:29:54 +0800

    555555gai

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
 $(function () {
 
+$(".chanyu").click(function(){
+    (".tc").removeciass("d-none")
+});
+    $(".tc").click(function(){
+        (".tc").addclass("d-none")
+    });
 
+    //$(".chanyu").click(function(){
+    //   window.location.href="../indextc.html"
+    //})
 
+//姓名开始
+// 姓名结束
 
+//手机号码开始
+//手机号码结束
 
-
+//生份证开始
+//生份证结束
 });
