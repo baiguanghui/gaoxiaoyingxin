5d63863fe31cdcd4093fcab1014a750ab25ed6c3
baiguanghui <724618014@qq.com>
2018-06-12 02:37:06 +0800

    12121212

--- js/index.js
+++ js/index.js
@@ -1,22 +1,16 @@
 $(function () {
 
-$(".chanyu").click(function(){
-    (".tc").removeciass("d-none")
-});
-    $(".tc").click(function(){
-        (".tc").addclass("d-none")
+// $(".chanyu").click(function(){
+//     (".tc").removeClass(".d-none")
+// });
+
+    $(".chanyu").click(function(){
+        (".tc").addClass('d-none')
     });
 
-    //$(".chanyu").click(function(){
+    // $(".chanyu").click(function(){
     //   window.location.href="../indextc.html"
-    //})
+    // })
 
-//姓名开始
-// 姓名结束
 
-//手机号码开始
-//手机号码结束
-
-//生份证开始
-//生份证结束
 });
